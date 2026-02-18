64b8129c62b32cd5fcd723def82773b4375a40a6
Victoria Muñoz <victoriamunozrobles@gmail.com>
2026-02-18 09:33:16 -0300

    add: getters & setters en la clase Tarea

--- src/TareaUrgente.java
+++ src/TareaUrgente.java
@@ -1,3 +1,8 @@
-public class TareaUrgente {
+public class TareaUrgente extends Tarea {
+    private String urgente;
 
+    public TareaUrgente(int id, String nombre, int prioridad, String completado, String urgente) {
+        super(id, nombre, prioridad, completado);
+        this.urgente = urgente;
+    }
 }
